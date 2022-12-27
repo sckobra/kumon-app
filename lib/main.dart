af33d62c6ab077d13ef369cf29291e23639a4111
Shreya Chati <shreyachati7@gmail.com>
2022-12-27 18:07:29 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,211 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+//import 'package:gsheets/gsheets.dart';
+import 'package:my_app/user_sheets_api.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await UserSheetsApi.init();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyNamePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyClockPage extends StatefulWidget {
+  const MyClockPage({super.key});
+
+  @override
+  State<MyClockPage> createState() => _MyClockPageState();
+}
+
+class _MyClockPageState extends State<MyClockPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('RecordDateTime'),
+        backgroundColor: const Color.fromARGB(255, 140, 203, 255),
+      ),
+      body: Center(
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(
+              height: 10,
+              width: 1,
+            ),
+            Row(
+              children: <Widget>[
+                const SizedBox(
+                  height: 1,
+                  width: 20,
+                ),
+                Text(
+                  "Hi, ${UserSheetsApi.names[UserSheetsApi.getCurrentIndex()]}!",
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 25),
+                ),
+                const SizedBox(
+                  height: 1,
+                  width: 100,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+              width: 1,
+            ),
+/*             const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+              ),
+            ),
+ */
+
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 100.0, vertical: 50.0),
+                  backgroundColor: const Color.fromARGB(255, 140, 203, 255),
+                  shape: const StadiumBorder(),
+                ),
+                onPressed: () {
+                  debugPrint("dialog opened - clock in");
+                  showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                            title: const Text(
+                                "Are you sure you want to check in?"),
+                            actions: [
+                              TextButton(
+                                  onPressed: () {
+                                    debugPrint("clock in confirmed");
+                                    UserSheetsApi.insertClockIn();
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Text("Yes"))
+                            ],
+                          ));
+                },
+                child: const Text('clock in')),
+            const SizedBox(
+              height: 20,
+              width: 1,
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 100.0, vertical: 50.0),
+                  backgroundColor: const Color.fromARGB(255, 140, 203, 255),
+                  shape: const StadiumBorder(),
+                ),
+                onPressed: () {
+                  debugPrint("dialog opened - clock out");
+                  showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                              title: const Text(
+                                  "Are you sure you want to check out?"),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    debugPrint("clock out confirmed");
+                                    UserSheetsApi.insertClockOut();
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Text("Yes"),
+                                )
+                              ]));
+                },
+                child: const Text('clock out')),
+          ],
+        ),
+      ),
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+    );
+  }
+}
+
+class MyNamePage extends StatefulWidget {
+  const MyNamePage({super.key});
+
+  @override
+  State<MyNamePage> createState() => _MyNamePageState();
+}
+
+class _MyNamePageState extends State<MyNamePage> {
+  final List<String> names = ['test'];
+
+  var currentName = "";
+
+  _MyNamePageState() {
+    UserSheetsApi.loadNames(this);
+    UserSheetsApi.loadDates(this);
+    //UserSheetsApi.fillTopRow(this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const title = "Sunnyvale Kumon Center";
+    debugPrint("names:$names");
+    debugPrint(UserSheetsApi.names[0]);
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text(title),
+            backgroundColor: const Color.fromARGB(255, 140, 203, 255),
+          ),
+          body: Center(
+              child: ListView.builder(
+            itemCount: UserSheetsApi.names.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: const Icon(Icons.circle),
+                title: Text(
+                  UserSheetsApi.names.isNotEmpty
+                      ? UserSheetsApi.names[index]
+                      : "test",
+                  textAlign: TextAlign.left, //this is default
+                  style: const TextStyle(fontSize: 17),
+                ),
+                trailing: const Icon(Icons.more_vert),
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 30.0, //margins from side of phone
+                  vertical: 12.0, //between list items
+                ),
+                onTap: () {
+                  debugPrint("name tapped");
+                  currentName = UserSheetsApi.names[index];
+                  UserSheetsApi.passCurrentIndex(index);
+                  debugPrint(
+                      "current index - accessed through class variable: ${UserSheetsApi.currentIndex}");
+                  debugPrint(currentName);
+
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const MyClockPage()),
+                  );
+                },
+              );
+            },
+          ))),
+    );
+  }
+}
---
+++ lib/user_sheets_api.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:gsheets/gsheets.dart';
+import 'package:intl/intl.dart';
+
+class UserSheetsApi {
+//credentials
+  static const _credentials = r'''
+{
+  "type": "service_account",
+  "project_id": "appsheet-362823",
+  "private_key_id": "1892bab6a5ddb40cb9ab7310ec5f841d9066fa6d",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDUgPFEgImtHxDq\nfX2WDhhvYAvBva7LpDZb7Ggh3i7grVwk+L0WyGoIM4O0PtWc99VufHiuc3h2kdlS\n4YnxL0YwGgu59oEPB2eTwwpg7QamcQaH6a5u/m76a7+vM5prmoo/8fJx0Is5bJ/+\n1NUU8FbOJ/sVwDzuDrF43ciiAq1n6jM0npN5X/eFJXGHZkTt9z5sXpbWVEg4okUt\nghfmkOniQjQ693owh/OnhaPwi9wtSPXlLMfW2PzpCEksUzEx0iTF+RmcZMY4m9yq\nMDt16zYE/ZFN1OEdU4HPrqI5YYeUcOFccwlTnNBubY/eale3PAfEwDcO+PJiL8Fx\ncdk1oQb/AgMBAAECggEAC1hdyS1fOSEGxN++emocb6gxW7G2HDgs3ZTn04fdrKNR\nkpKKSD+aumWQFNAjLm/kcxm6uOTNG/BAdomeodCtm7htEAuzKzhXFTWQbC+4L0Wq\n6bGvqhknMQzbA7Sqr2KYs/F4yXViIXnSEIeQLFlPTeiNId9ADlBpgc1X2B8/EjEy\nGqE6h5/rcQ/8d6lHV0UPCcsqN5AWaH1lznAHwhkaYBbCG7h4sI8GqfUwjYkCkqdf\nTQna9diYKHJdnp/zn8QGyR/rvsF+j8y1/P1tfdHSx2wsel5k4FHdVTOiJ4+YGkaI\nBTa8xpW5uybMB0l75o3S07ChpoL8FgtD4YwpLBkQMQKBgQD32nSUokRQWGfYYWCC\nGa3IC5ZWcmWZW/R+My8xobh8C2NRHqeZZg1Bv9Q1Fmd7p3+rCBtgUR8W0CV2nfZL\nen3UL0YlbN4iyUjuxOKJRw1glq1kt8+7wtPGh9I3JUbnq6KKqJrE+8lnQrS/KEZ6\ncaVwkgGVUvpd/glnFkrp/fjR0QKBgQDbfQoxd5Yc+RzdhL84wSHVz8+plhC1EXIS\n/pvRHzY1Kmix+zS5ubkgLosBaofB8/EGWapSM6UL1yjO3oaGblP9NmIOe7BOGho0\nZfUI9i0yYoAXJxLNfNUruXX1w8spcQQyRRqi8ASdGwffWIfiChpGomJYQDAc9AMX\nrdkv3MovzwKBgQChcOn+5rMYon0BtGvoUlGfu+xw4pLLEdpvWWekLHK+WayXaiBB\nCvgXm5vd+HnvUWsXPHCYjD5z3aCJYtrKzz6mtyQ5vG/0uKeI3zWvtVhBUeQfdpsY\n8g5/gniq8tz8ig+Vwb0OEfNeD1UsLk+58cyRRolBeosaLh5xx1OElqk8AQKBgQC4\nkHOy1xF2uY+pQogdrGHMTlwcCaFw8QOQr2LNPcayfD/fchKLHn5qbljbTv5rfLqv\nkVSknOLrivi4w+wxPLMJmOJptV6yvkZMy3N6HrAwzx1Q2r1358HFa8WibQr4hR13\nPB7C5ruqdGFxURuqWr5vqPqzpP4QSqwTDFA2bB3khwKBgGORfl17Ab+nUCPt6qQ6\n5sKnHc9GMaXdalCDUQBhraZKA7dQS7R/k2YCpFNruFXGsadajxofoyI5b6OlJJnJ\nQLHzYEhnOzxRCVUmvY8XL1yuyMyHFeEcAlzacle7kdgOcTVq9yan4pIVTEDqpcNJ\noZtw+ww5FkJiPr3GlJAD9REI\n-----END PRIVATE KEY-----\n",
+  "client_email": "appsheet@appsheet-362823.iam.gserviceaccount.com",
+  "client_id": "106744706967040456442",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/appsheet%40appsheet-362823.iam.gserviceaccount.com"
+}
+
+''';
+  static final _spreadsheetId = '1Y7MM6ztyr4Rvb_Z4k9w_REb1xM4UXZtj0Y0jgPUYtKs';
+
+  static final gsheets = GSheets(_credentials);
+  static Worksheet? _userSheet;
+  static Worksheet? _timeSheet;
+
+  static Future init() async {
+    final spreadsheet = await gsheets.spreadsheet(_spreadsheetId);
+    _userSheet = await _getWorkSheet(spreadsheet, title: 'Employees');
+    _timeSheet =
+        await _getWorkSheet(spreadsheet, title: 'Copy of September2022');
+  }
+
+  static Future<Worksheet> _getWorkSheet(
+    Spreadsheet spreadsheet, {
+    required String title,
+  }) async {
+    //checks if the spreadsheet exists
+    try {
+      return await spreadsheet.addWorksheet(title);
+    } catch (e) {
+      return spreadsheet.worksheetByTitle(title)!;
+    }
+  }
+
+  static List<String> names = ['xyz'];
+  static int currentIndex = 0;
+
+  static List<String> dates = ['abc'];
+
+  static List<String> topRow = ['Date'];
+
+  static List<String> getNames() {
+    return names;
+  }
+
+  static f(List<String> value, State state) {
+    names = value;
+    //topRow = names;
+    topRow.add('Day Total');
+    topRow.add('Day Pay');
+    for (int i = 0; i < names.length; i++) {
+      topRow.add('${names[i]} Check In');
+      topRow.add('${names[i]} Check Out');
+      topRow.add('${names[i]} Total Time');
+      topRow.add('${names[i]} Pay');
+    }
+
+    debugPrint('$topRow');
+    _timeSheet!.clearRow(1);
+    _timeSheet!.values.insertRow(1, topRow);
+
+    state.setState(() {});
+  }
+
+  static void loadNames(State state) {
+    debugPrint("calling loadNames");
+    Future<List<String>> n = _userSheet!.values.column(1, fromRow: 2);
+    n.then((value) => f(value, state));
+  }
+
+  static void g(List<String> value, State state) {
+    dates = value;
+    state.setState(() {});
+
+    debugPrint("dates are $dates");
+  }
+
+  static void loadDates(State state) {
+    debugPrint("calling loadDates");
+    //Future<List<String>> formattedDates = [] as Future<List<String>>;
+    Future<List<String>> d = _timeSheet!.values.column(1, fromRow: 2);
+
+    d.then((value) => g(value, state));
+  }
+
+  static String getCurrentHourMin() {
+    final currentTime = DateTime.now();
+    return DateFormat.jm().format(currentTime);
+  }
+
+  static String getMonthDayYear() {
+    final currentTime = DateTime.now();
+    return DateFormat.yMd().format(currentTime);
+  }
+
+  static void insertClockIn() {
+    for (int i = 0; i < dates.length; i++) {
+      var currentMonthDayYear = getMonthDayYear();
+      if (dates[i] == currentMonthDayYear) {
+        debugPrint(dates[i]);
+        int currentDateIndex = i;
+        debugPrint('${names[currentIndex]} current name pressed $currentIndex');
+        var rowIndexName = topRow.indexOf('${names[currentIndex]} Check In');
+        debugPrint('$rowIndexName');
+        _timeSheet!.values.insertValue(getCurrentHourMin(),
+            column: rowIndexName + 1, row: currentDateIndex + 2);
+      }
+    }
+  }
+
+  static void insertClockOut() {
+    for (int i = 0; i < dates.length; i++) {
+      if (dates[i] == getMonthDayYear()) {
+        debugPrint(dates[i]);
+        int currentDateIndex = i;
+        debugPrint('${names[currentIndex]} current name pressed $currentIndex');
+        var rowIndexName = topRow.indexOf('${names[currentIndex]} Check Out');
+        debugPrint('$rowIndexName');
+        _timeSheet!.values.insertValue(getCurrentHourMin(),
+            column: rowIndexName + 1, row: currentDateIndex + 2);
+      }
+    }
+  }
+
+  static void passCurrentIndex(var index) {
+    //debugPrint("current index is: $index");
+    currentIndex = index;
+  }
+
+  static int getCurrentIndex() {
+    return currentIndex;
+  }
+}
